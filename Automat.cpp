ebd83595cde0daca70b7138f091345fd645858e5
IXOFIXOF <IXOF@POCZTA.ONET.PL>
2018-03-17 19:58:45 +0100

    Dodanie obsługi git spod Visual studio

--- Automat.cpp
+++ Automat.cpp
@@ -13,6 +13,26 @@ void CAutomat::Init()
 	cout << "1. Stan automatu" << endl;
 	cout << "2. Dodaj produkt" << endl;
 	cout << "3. Edytuj produkt" << endl;
+	cin >> wybor;
+	switch (wybor)
+	{
+	case 1:
+	{
+
+		break;
+	}
+	case 2:
+	{
+		break;
+	}
+	case 3:
+	{
+		break;
+	}
+	default:
+		cout << "Dokona³es nieprawidlowego wyboru" << endl;
+		break;
+	}
 
 }
 CAutomat::~CAutomat()
